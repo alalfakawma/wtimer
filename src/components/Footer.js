fdcecf71908a77035678ca946eea35b22b4d00b8
alalfakawma <alalfakawma@live.com>
2019-01-09 15:38:20 +0530

    Updated the readme file and Footer Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import moment from 'moment';
 
 const Footer = () => (
-	<div className="has-text-light footer-bottom">Made with ❤️ by Aseem Lalfakawma - { moment().format('YYYY') }</div>
+	<div className="has-text-light footer-bottom">Made with <span role="img" aria-label="Heart">❤️</span> by Aseem Lalfakawma - { moment().format('YYYY') }</div>
 );
 
 export default Footer;
